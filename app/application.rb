b87e52badd13d496cfcb12c4941d8423559c888b
Stephanie Abud <sabud1208@gmail.com>
2019-01-28 03:00:00 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -5,7 +5,7 @@ class Application
   time = Time.now.to_i
   t = Time.now.hour
   resp.write "#{t}\n"
-  if t == 1200
+  if t >= 0000
     resp.write "Good Morning!"
   elsif t <= 1300
     resp.write "Good Afternoon!"
