5fb152dd10bc0f8b8ec98ffd9e7697a60904850d
Stephanie Abud <sabud1208@gmail.com>
2019-01-28 02:40:41 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -3,7 +3,7 @@ class Application
   def call(env)
   resp = Rack::Response.new 
   time = Time.new(2015,11,27,9,30)
-  if Time.now.to_i > time
+  if Time.now.to_i == time
     resp.write "Good Morning!"
   else 
     resp.write "Good Afternoon!"
