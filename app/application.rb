495c2a7f8fbafd692ed15dd02e70bb56e01ed29c
Stephanie Abud <sabud1208@gmail.com>
2019-01-28 02:58:00 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -3,11 +3,11 @@ class Application
   def call(env)
   resp = Rack::Response.new 
   time = Time.now.to_i
-  t = Kernel.rand(1..12)
+  t = Time.now.hour
   resp.write "#{t}\n"
-  if t <= 1200
+  if t >= 1200
     resp.write "Good Morning!"
-  elsif t >= 1200 
+  elsif t == 1200 
     resp.write "Good Afternoon!"
   end
    resp.finish
